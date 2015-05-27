d372ca4d1fea04b74bc18bfc2ede41806122a0a9
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-05-27 14:07:39 -0700

    msm8916: Add CEC support for 8916
    Enable CEC HAL to integrate CEC framework and CEC hardware.
    
    Change-Id: Iaa30d3f67beabf18d5a22e8a5a147b92889dca7b
    
    Conflicts:
    	system.prop

---
+++ dev_qcom_msm8916_nov_release.sh
@@ -0,0 +1,5 @@
+#CEC CHANGES
+git fetch ssh://review-android.quicinc.com:29418/platform/vendor/qcom/msm8916 refs/changes/46/1287646/3 && git cherry-pick FETCH_HEAD
+
+#ADSP CHANGES
+git fetch ssh://review-android.quicinc.com:29418/platform/vendor/qcom/msm8916 refs/changes/61/1425261/1 && git cherry-pick FETCH_HEAD
